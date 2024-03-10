442df581f06cee38fb799a01046b659ed422575e
Zayka Lyudmyla <103557961+ludzay@users.noreply.github.com>
2024-03-10 16:44:12 +0200

    Update 1-gallery.js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,4 +1,4 @@
-import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.css';
 const images = [
